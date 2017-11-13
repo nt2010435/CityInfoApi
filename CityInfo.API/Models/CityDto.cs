08a5f67efc6d1a2fef14210db2729857422e44bf
nt2010435 <nt2010435@outlook.com>
2017-11-12 23:27:54 -0800

    Returning child resources from api

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CityInfo.API.Models
 {
     public class CityDto
@@ -7,6 +9,13 @@ namespace CityInfo.API.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest { get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } 
+        = new List<PointOfInterestDto>();
     }
 }
