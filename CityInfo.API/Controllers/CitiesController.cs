80de95de2da82a4d1771aca1c067e098839a0214
nt2010435 <nt2010435@outlook.com>
2017-11-12 22:30:02 -0800

    Initial check-in of module CityInfo.API

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+
+            //return new JsonResult(new List<object>()
+            //{
+            //    new { id=1, Name="New York City"},
+            //    new { id=2, Name="Antwerp"}
+            //});
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+            );
+        }
+    }
+}
